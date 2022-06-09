518285eba8bb0d8d7afa8ce8e9a907d044a8e541
ashley-lau <ashleylau451@gmail.com>
2022-06-09 15:01:33 +0800

    Initial commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyProject.Controllers;
+
+[Route("api/[controller]")]
+public class ValuesController : ControllerBase
+{
+    // GET api/values
+    [HttpGet]
+    public JsonResult GetAllValues()
+    {
+        return new JsonResult(new string[] { "Mister", "Naveed" });
+    }
+
+    // GET api/values/Helloworld
+    [HttpGet("HelloWorld")]
+    public JsonResult HelloWorld()
+    {
+        return new JsonResult("Hello world! You are hitting a aws lambda webserver api!");
+    }
+
+    // GET api/values/5
+    [HttpGet("{id}")]
+    public JsonResult Get(int id)
+    {
+        return new JsonResult(id.ToString());
+    }
+}
